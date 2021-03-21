6fdd9baf41c0e5c887260518cbadb57c4ef31023
BirkanAk <bak1@binghamton.edu>
2021-03-21 21:40:45 +0300

    The view is done! Now to the hard part...

--- app/src/main/java/com/example/birkanakinterestinapptts/MainActivity.java
+++ app/src/main/java/com/example/birkanakinterestinapptts/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.birkanakinterestinapptts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button TTSButton;
+    EditText TTSText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onTTSClick(View view) {
     }
 }
